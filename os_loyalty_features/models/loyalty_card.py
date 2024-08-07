4c5d2ca61d467f39d8b0c73455c988de141b1d3a
dziadyk <dziadyk_v@ukr.net>
2024-08-07 14:03:29 +0300

    new features

--- os_loyalty_features/models/loyalty_card.py
+++ os_loyalty_features/models/loyalty_card.py
@@ -1,7 +1,24 @@
 from odoo import _, api, exceptions, fields, models
+from datetime import datetime, timedelta
 
 
-class Project(models.Model):
-    _inherit = 'project.project'
+class LoyaltyCard(models.Model):
+    _inherit = 'loyalty.card'
 
-    os_department_id = fields.Many2one(related='user_id.employee_id.department_id', string='Department', store=True, readonly=True)
+#    points = fields.Float(
+#        default=100,
+#        tracking=True,
+#    )
+#    expiration_date = fields.Date(
+#        default=datetime.now() + timedelta(days=365),
+#    )
+
+    @api.model
+    def default_get(self, fields):
+        defaults = super(LoyaltyCard, self).default_get(fields)
+        defaults.update(
+            points=100,
+            expiration_date=datetime.now() + timedelta(days=365),
+        )
+
+        return defaults
