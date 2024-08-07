3b9de604ea8e020ba2316d46bac94c2ccaa129f1
dziadyk <115286823+dziadyk@users.noreply.github.com>
2024-08-07 10:40:44 +0300

    Add files via upload

---
+++ os_loyalty_features/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Loyalty (OSC Features)',
+    'summary': 'Loyalty (OSC Features)',
+    'description': 'Loyalty (OSC Features)',
+
+    'author': 'Volodymyr Dziadyk',
+    'website': 'https://www.oneservice-consulting.com/',
+    'support': 'dvol@oneservice.in.ua',
+
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'version': '17.0.1.0.1',
+
+    'depends': [
+        'loyalty',
+    ],
+
+    'data': [
+    #    'views/project_filter_views.xml',
+    #    'views/project_form_views.xml',
+    ],
+
+    'demo': [
+    ],
+
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+
+}
