d7b5a106f1071e01af467410e0bdadb7dfe203cb
dziadyk <dziadyk_v@ukr.net>
2024-08-08 17:20:01 +0300

    new features

--- os_loyalty_features/models/loyalty_card.py
+++ os_loyalty_features/models/loyalty_card.py
@@ -5,20 +5,19 @@ from datetime import datetime, timedelta
 class LoyaltyCard(models.Model):
     _inherit = 'loyalty.card'
 
-#    points = fields.Float(
-#        default=100,
-#        tracking=True,
-#    )
-#    expiration_date = fields.Date(
-#        default=datetime.now() + timedelta(days=365),
-#    )
-
     @api.model
     def default_get(self, fields):
         defaults = super(LoyaltyCard, self).default_get(fields)
         defaults.update(
-            points=100,
+            points=999,
             expiration_date=datetime.now() + timedelta(days=365),
         )
 
         return defaults
+
+    def write(self, vals):
+        if 'points' in vals:
+            if vals['points'] == 1:
+                vals['points'] = 999
+        res = super(LoyaltyCard, self).write(vals)
+        return res
