87a758b1737912e2fcd7e936d141ddd5c43fbcf1
dziadyk <dziadyk_v@ukr.net>
2024-08-16 16:59:44 +0300

    new features

--- os_loyalty_features/models/res_partner.py
+++ os_loyalty_features/models/res_partner.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    #birthday = fields.Date(
-    #    string='Date of Birth'
-    #)
+    birthday = fields.Date(
+        string='Date of Birth'
+    )
